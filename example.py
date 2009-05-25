a24991b3ad72adf2374fd64ed9ba577653305e46
Simon Willison <simon@simonwillison.net>
2009-05-25 20:56:46 +0100

    Change syntax to account for different types

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import optfunc
 
 @optfunc.run
-def foo(filename, v = optfunc.Switch('-v', '--version')):
+def foo(filename, v = optfunc.String('-v', '--version')):
     "Usage: %s foo bar - do something useful"
     pass
--- optfunc.py
+++ optfunc.py
@@ -1,10 +1,21 @@
 from optparse import OptionParser
 
-class Switch(object):
+class Var(object):
     def __init__(self, shorthand, longhand, default=None):
         self.shorthand = shorthand
         self.longhand = longhand
         self.default = default
+    
+    def convert(self, data):
+        return data
+
+class String(Var):
+    def convert(self, data):
+        return str(data)
+
+class Bool(Var):
+    def convert(self, data):
+        return data.strip() not in ('0', '', 'false', 'False')
 
 def run(func):
     # TODO: introspect function, build up command line args, execute it
